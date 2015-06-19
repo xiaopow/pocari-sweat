305537a35dbd0316e1fcdce84bb29892029ca888
Victoria Li <wingman.li@hotmail.com>
2015-06-19 17:05:52 +0800

    started notification

---
+++ server/notification.js
@@ -0,0 +1,31 @@
+var apn = Meteor.require("apn"),
+    path = Npm.require('path'),
+    apnOptions = Meteor.settings.apnOptions || {},
+    alertSound = apnOptions.sound || "alert.aiff",
+    apnConnection
+
+// default apn connection options
+apnOptions = _.extend({
+  cert: path.join(appRootPath, "private", "cert.pem"),
+  key: path.join(appRootPath, "private", "key.pem"),
+}, apnOptions)
+apnConnection = new apn.Connection(apnOptions)
+
+Meteor.methods({
+  sendAppleNotifications: function (alert, url, pushIds) {
+    var note = new apn.Notification()
+
+    // expires 1 hour from now
+    note.expiry = Math.floor(Date.now() / 1000) + 3600
+    note.sound = alertSound
+    note.alert = alert
+    note.payload = {'url': url}
+
+    _.each(pushIds, function (token) {
+      var device = new apn.Device(token)
+      apnConnection.pushNotification(note, device)
+    })
+
+    return {success:'ok'}
+  }  // end sendAppleNotifications
+})
