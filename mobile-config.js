2a14343e0437c49fac0df6d3ed834204267569c2
Xiaopow <xiaoyang88@gmail.com>
2015-06-21 03:59:15 +0800

    add oswald font

--- mobile-config.js
+++ mobile-config.js
@@ -11,7 +11,7 @@ App.icons({
   // iOS
   'iphone': 'resources/icons/icon-60x60.png',
   'iphone_2x': 'resources/icons/icon-60x60@2x.png',
-  'iphone6': 'resources/icons/icon-180x180.png',
+  'iphone_3x': 'resources/icons/icon-180x180.png',
   'ipad': 'resources/icons/icon-72x72.png',
   'ipad_2x': 'resources/icons/icon-72x72@2x.png',
 
@@ -24,7 +24,6 @@ App.icons({
 
 App.launchScreens({
   // iOS
-  'iphone': 'resources/splash/splash-320x480.png',
   'iphone_2x': 'resources/splash/splash-320x480@2x.png',
   'iphone5': 'resources/splash/splash-320x568.png',
   'iphone6': 'resources/splash/splash-375x667.png',  
@@ -32,17 +31,7 @@ App.launchScreens({
   'ipad_portrait': 'resources/splash/splash-768x1024.png',
   'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
   'ipad_landscape': 'resources/splash/splash-1024x768.png',
-  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
-
-  // Android
-  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png'
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
--- server/notification.js
+++ server/notification.js
@@ -20,7 +20,6 @@ if (Meteor.isServer) {
       var timeNow = moment(date).format("h:mm A");
       var reminderItems = Reminders.find({time: timeNow, on: true});
       console.log(timeNow);
-      console.log(reminderItems);
       reminderItems.forEach(function(reminderItem) {
         console.log("send to user" + reminderItem.userId);
         Meteor.call("serverNotification",reminderItem.userId);
